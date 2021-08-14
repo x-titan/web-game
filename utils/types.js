230ede01526f7cb7b7367e525c7462b5495ff23d
Aset Telmanov <telmanov2002.at@gmail.com>
2021-08-14 13:04:02 +0300

    republish

--- utils/types.js
+++ utils/types.js
@@ -8,15 +8,24 @@ export default Object.freeze({
   type(value, type) { return type === typeof value },
   /** @param {new} object */
   instance(value, object) { return value instanceof object },
+  /** @returns {value is (undefined | null)} */
   empty(value) { return value === undefined || value === null },
+  /** @returns {value is number} */
   num(value) { return "number" === typeof value || value instanceof Number },
+  /** @returns {value is string} */
   str(value) { return "string" === typeof value || value instanceof String },
+  /** @returns {value is object} */
   obj(value) { return "object" === typeof value && value instanceof Object },
+  /** @returns {value is boolean} */
   bool(value) { return "boolean" === typeof value || value instanceof Boolean },
+  /** @returns {value is Function} */
   func(value) { return "function" === typeof value && value instanceof Function },
+  /** @returns {value is Array} */
   array(value) { return Array.isArray(value) },
+  /** @returns {value is NaN} */
   NaN(value) { return "number" === typeof value && isNaN(value) },
-  /** Returns "TRUE" if the element being checked is an instance of the class */
-  nonZeroValue(value) { return !(value === undefined || value === null || value === 0 || NaN(value)) },
+  /** @returns {value is (undefined | null | 0 | NaN)} */
+  nonZeroValue(value) { return !(value === undefined || value === null || value === 0 || is.NaN(value)) },
+  /** @returns {value is (undefined | null | globalThis)} */
   notClass(value) { return value === undefined || value === null || value === globalThis }
 })
